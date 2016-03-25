35233897c22d820dc54c5b7eb00b89eff5fa4d86
Langelihle Ngobese <langelihlengobese@gmail.com>
2016-03-25 03:26:25 +0200

    Complete answer

--- Question4.cpp
+++ Question4.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
+// Name:Langelihle Ngobese
+// Student No:213571644
 // Date: 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -11,3 +11,57 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Animal{
+public:
+	virtual void name(){
+		cout << "I am a animal" << endl;
+	}
+	virtual void does(){
+		cout << "I can do many actions"<< endl;
+	}
+
+};
+
+class Bird :public Animal{
+public:
+	void name(){
+		cout << "I am a bird." << endl;
+	}
+
+	void does(){
+		cout << "I fly and chirp." << endl;
+	}
+};
+
+class Dog :public Animal{
+public:
+	void name(){
+		cout << "I am a dog" << endl;
+	}
+
+	void does(){
+		cout << "I run and bark." << endl;
+	}
+};
+
+int main(){
+	Animal anil;
+	Bird andy;
+	Dog anne;
+
+	anil.name();
+	anil.does();
+
+	andy.name();
+	andy.does();
+
+	anne.name();
+	anne.does();
+
+	return 0;
+}
